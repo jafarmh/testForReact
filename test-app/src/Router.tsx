ca545f0b4a37ed9de0ea4b8ea62bbd55281a97ad
jafarmh <jafar.m@yahoo.com>
2023-12-26 20:06:24 +0330

    add router

---
+++ test-app/src/Router.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import { Routes } from "react-router-dom";
+
+export default function Router() {
+    return (
+        <Routes>
+
+
+        </Routes>
+
+    );
+}
+
+
