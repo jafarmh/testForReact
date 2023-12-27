16e2e8ca46fc447f44582c69105ae75e43b6071f
jafarmh <jafar.m@yahoo.com>
2023-12-27 21:52:21 +0330

    add index

--- test-app/src/Router.tsx
+++ test-app/src/Router.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import Index from "./pages/Index";
+import { Layout } from "./layout/Layout";
 
 export default function Router() {
     return (
         <Routes>
 
-
+            <Route path={"/"} element={<Layout />} >
+                <Route index element={<Index />} />
+                </Route>
         </Routes>
 
     );
